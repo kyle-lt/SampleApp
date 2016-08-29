8f22ad3e99e80b46f9a614e57f1bb31aed777cec
Mark Prichard <mqprichard@gmail.com>
2016-08-29 11:42:17 -0700

    Added Jennifer's changes to pick up app id

--- web/src/public/js/controllers/MainController.js
+++ web/src/public/js/controllers/MainController.js
@@ -11,12 +11,9 @@
             $location,
             CONTROLLER_URL,
             CONTROLLER_PORT,
-            CONTROLLER_SSL) {
+            CONTROLLER_SSL,
+            APP_ID) {
 
-        // TODO add these constants to above
-        // CONTROLLER_USER,
-        //     CONTROLLER_ACCOUNT,
-        //     CONTROLLER_PWD
             $scope.ready = false;
 
             $scope.init = function () {
@@ -51,34 +48,9 @@
                         break;
                 }
 
-                var baseUrl = 'http://'+ CONTROLLER_URL + ":" + CONTROLLER_PORT + "/controller";
-                var basicAuthString = CONTROLLER_USER + "@" + CONTROLLER_ACCOUNT + ":" + CONTROLLER_PWD;
-
-                // TODO these are used for testing. delete later
-                // var baseUrl = "http://localhost:8080/controller";
-                // var basicAuthString = "user1@customer1:welcome";
-                // var url= "http://localhost:8080/controller/#/location=" + location + "&application=" + 10;
-
-                // TODO debug this. Curl in javascript is an AJAX call. It is currently returning a cross origin error
-                //
-                $.ajax({
-                    url: baseUrl + "/restui/applicationManagerUiBean/applicationByName?applicationName=SampleApp",
-                    dataType: 'application/json',
-                    type: 'get',
-                    beforeSend: function(xhr) {
-                      xhr.setRequestHeader("Authorization", "Basic" + btoa(basicAuthString))
-                    },
-                    success: function(data) {
-                        var appId = data.id;
-                        var s = CONTROLLER_SSL == "true" ? "s" : "" ;
-                        var url = "http" + s + "://" + CONTROLLER_URL + ":" + CONTROLLER_PORT + "/#/location=" + location + "&application=" + appId;
-                        window.open(url, "AppDynamicsController");
-
-                    },
-                    error: function(data) {
-
-                    }
-                });
+                var s = CONTROLLER_SSL == "true" ? "s" : "" ;
+                var url = "http" + s + "://" + CONTROLLER_URL + ":" + CONTROLLER_PORT + "/#/location=" + location + "&application=" + APP_ID;
+                window.open(url, "AppDynamicsController");
             };
         
             $scope.init();
