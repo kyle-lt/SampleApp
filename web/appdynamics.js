2bb0444c9beca3790593ec22495c0fe45fb39b7a
Kyle Tully <kyle-lt@users.noreply.github.com>
2019-03-12 13:51:50 -0500

    edited appd config

--- web/appdynamics.js
+++ web/appdynamics.js
@@ -1,3 +1,3 @@
 require("appdynamics").profile({
-                libagent: 'true'
-        });
+	libagent: 'true'
+});
--- web/start-all.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-echo "Starting Node.js web server"
-nohup node SampleApp/src/server.js > nodejs.log 2>&1&
-
-exit 0
